7880d8ae981a0bf41bcb12a05b2e1b50f5fab80e
CarlosBridi <carlosbridi@gmail.com>
2018-06-27 23:00:20 -0300

    Animações nos compoentens de login e no car de imagens

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, trigger, state, style } from '@angular/core';
+import { Component, OnInit, trigger, state, style, animate, transition } from '@angular/core';
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
@@ -11,17 +12,50 @@ import { Component, OnInit, trigger, state, style } from '@angular/core';
       })), 
       state('visivel', style({
         opacity: 1
-      }))
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in'))
     ]) 
   ]
 })
 export class BannerComponent implements OnInit {
 
   public estado: string = 'escondido';
+  public TEMPO_TIMEOUT: number = 3000
+
+  public imagens: Imagem[] = [
+    { estado:'visivel', url: './assets/banner-acesso/img_1.png' },
+    { estado:'escondido', url: './assets/banner-acesso/img_2.png' },
+    { estado:'escondido', url: './assets/banner-acesso/img_3.png' },
+    { estado:'escondido', url: './assets/banner-acesso/img_4.png' },
+    { estado:'escondido', url: './assets/banner-acesso/img_5.png' }
+  ]
 
   constructor() { }
 
+
+
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), this.TEMPO_TIMEOUT);
+  }
+
+  public logicaRotacao(): void {
+    let idx: number;
+
+    for (let i: number = 0; i <= 4; i++){
+      if (this.imagens[i].estado === 'visivel'){
+        this.imagens[i].estado = 'escondido';
+
+        idx = i === 4 ? 0 : i + 1;
+
+        break;
+      }
+
+    }
+
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), this.TEMPO_TIMEOUT);
+
   }
 
 }
--- src/app/acesso/login/login.component.ts
+++ src/app/acesso/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  trigger, state , style,transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  public estadoBanner: string = 'criado';
 
   constructor() { }
 
